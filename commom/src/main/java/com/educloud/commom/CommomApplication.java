0a06884b9a62226fc1c7bc28583c562ce0cea126
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2026-01-16 14:21:47 -0500

    add base API response wrapper and custom business exception

---
+++ commom/src/main/java/com/educloud/commom/CommomApplication.java
@@ -0,0 +1,13 @@
+package com.educloud.commom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommomApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hi commom");
+    }
+
+}
